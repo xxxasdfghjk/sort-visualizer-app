1b2c9e6b9a956905555a179edb8fb50c69710350
xxxasdfghjk <xxxasdfghjk999@gmail.com>
2024-03-11 08:26:10 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    esbuild: {
+        target: "esnext",
+        platform: "browser",
+    },
     plugins: [react()],
     base: "/sort-visualizer-app/",
 });
